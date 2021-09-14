db83ab88b88d1310f32472767ea963bdf222768d
vvsoggy <vvsogi@gmail.com>
2021-09-14 15:33:39 +0900

    [docs] 클라이언트 환경 구축

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,12 @@
-import React from 'react';
-import './App.css';
+import Box from "pages/box/box";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <h1>Hello World!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box />
       </header>
     </div>
   );
