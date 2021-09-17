15ce725f6f6877f1933d8908beba43243d354f59
박민수 <pinion7rl@gmail.com>
2021-09-17 11:49:40 +0900

    [feat] 회원가입 api 구현 충돌수정

--- server/src/me/me.controller.ts
+++ server/src/me/me.controller.ts
@@ -1,42 +1,26 @@
 import {
   Controller,
-  Get,
+  Headers,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  ValidationPipe,
+  Get,
 } from '@nestjs/common';
 import { MeService } from './me.service';
-import { CreateMeDto } from './dto/create-me.dto';
-import { UpdateMeDto } from './dto/update-me.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('me')
+@Controller()
 export class MeController {
   constructor(private readonly meService: MeService) {}
-
-  @Post()
-  create(@Body() createMeDto: CreateMeDto) {
-    return this.meService.create(createMeDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.meService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.meService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMeDto: UpdateMeDto) {
-    return this.meService.update(+id, updateMeDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.meService.remove(+id);
+  @Post('signup')
+  async signUp(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<string> {
+    await this.meService.signUp(createUserDto);
+    return Object.assign({
+      data: { ...createUserDto },
+      statusCode: 201,
+      statusMsg: `saved successfully`,
+    });
   }
 }
--- server/src/me/me.module.ts
+++ server/src/me/me.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MeService } from './me.service';
 import { MeController } from './me.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [MeController],
   providers: [MeService],
 })
