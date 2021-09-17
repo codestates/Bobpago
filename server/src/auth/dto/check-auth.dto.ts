b3a4a16822001979abaf9b296454eee0b3f17671
박민수 <pinion7rl@gmail.com>
2021-09-17 11:03:26 +0900

    [feat] 로그인, 로그아웃, 새로운 토큰 발급

---
+++ server/src/auth/dto/check-auth.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CheckAuthDto {
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
