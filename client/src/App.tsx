08a09f8a53a04aa75a8fdfac5d8685950a81d0d3
박민수 <pinion7rl@gmail.com>
2021-09-19 17:51:46 +0900

    dwvw

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import SignUp from "pages/SignUpAndSignIn/SignUp";
 import WriteRecipe from "pages/WriteRecipe/WriteRecipe";
 import { useDispatch } from "react-redux";
 import { showSignIn } from "actions/SignUpAndSignIn";
-import { LogoRow } from "components/SignUpAndSignIn/LogoRow";
+// import { LogoRow } from "components/SignUpAndSignIn/LogoRow";
   
 function App() {
   const dispatch = useDispatch();
