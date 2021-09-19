1cc395560c86841f28e8a1316f94e57817a69e8d
박민수 <pinion7rl@gmail.com>
2021-09-19 17:42:09 +0900

    oauth 구현 시도중

---
+++ server/src/middleware/auth-checker.middleware.ts
@@ -0,0 +1,9 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class AuthCheckerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    next();
+  }
+}
