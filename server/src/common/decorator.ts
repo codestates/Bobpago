079b214921072cf009681405be10bee8c53369ac
박민수 <pinion7rl@gmail.com>
2021-09-22 21:51:33 +0900

    [feat] 카카오 및 네이버 소셜 로그인 구현 resolve #45

--- server/src/common/decorator.ts
+++ server/src/common/decorator.ts
@@ -1,13 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 
-export const GetCookie = createParamDecorator(
-  (data, ctx: ExecutionContext): { refreshToken?: string } => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.cookies;
-  },
-);
-
 export const GetUser = createParamDecorator(
   (data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
