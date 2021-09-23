f4e3ff33a31033049418c6e5e2c37ee7059dd2de
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-09-23 16:51:56 +0900

    [fix] presigned url service logic

--- server/src/image/image.controller.ts
+++ server/src/image/image.controller.ts
@@ -1,82 +1,20 @@
 import {
   Controller,
-  Post,
-  Get,
   UploadedFiles,
   UseInterceptors,
+  Put,
 } from '@nestjs/common';
-import {
-  FileFieldsInterceptor,
-  FilesInterceptor,
-} from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { ImageService } from './image.service';
-import * as multerS3 from 'multer-s3';
-import * as AWS from 'aws-sdk';
 import 'dotenv/config';
-
-const s3 = new AWS.S3();
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
-  @Post('upload')
-  @UseInterceptors(
-    FileFieldsInterceptor(
-      [
-        { name: 'f1', maxCount: 3 },
-        { name: 'f2', maxCount: 3 },
-      ],
-      {
-        storage: multerS3({
-          s3: s3,
-          bucket: process.env.AWS_S3_BUCKET_NAME,
-          contentType: multerS3.AUTO_CONTENT_TYPE,
-          acl: 'private',
-          key: function (req, file, cb) {
-            cb(null, file.originalname);
-          },
-        }),
-      },
-    ),
-  )
-  async uploadImage(@UploadedFiles() files: Express.Multer.File) {
-    console.log(files);
-    await this.imageService.uploadImage(files['f1'][0]);
-    return files;
-  }
-  @Get('presigned')
-  @UseInterceptors(
-    FilesInterceptor('files', 3, {
-      storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_S3_BUCKET_NAME,
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: 'private',
-        key: function (req, file, cb) {
-          cb(null, file.originalname);
-        },
-      }),
-    }),
-  )
-  async getSignedUrl(@UploadedFiles() files: Express.Multer.File) {
+  @Put('presigned')
+  @UseInterceptors(FilesInterceptor('files'))
+  async getUrl(@UploadedFiles() files: Express.Multer.File) {
     return this.imageService.presignedPUTURL(files);
-  }
-  @Post('presigned')
-  @UseInterceptors(
-    FilesInterceptor('files', 3, {
-      storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_S3_BUCKET_NAME,
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: 'private',
-        key: function (req, file, cb) {
-          cb(null, file.originalname);
-        },
-      }),
-    }),
-  )
-  async presignedPUTURL(@UploadedFiles() files: Express.Multer.File) {
-    return this.imageService.presignedGETURL(files);
   }
 }
--- server/src/image/image.service.ts
+++ server/src/image/image.service.ts
@@ -12,54 +12,19 @@ const s3 = new AWS.S3();
 
 @Injectable()
 export class ImageService {
-  async uploadImage(file) {
-    console.log(file);
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      ACL: 'private',
-      Key: `path/${file.originalname}.png`,
-      Body: file.buffer,
-    };
-    try {
-      const s3Response = await s3.upload(params).promise();
-
-      console.log(s3Response);
-    } catch (e) {
-      console.log(e);
+  async presignedPUTURL(files) {
+    const urls = [];
+    for (const file of files) {
+      const data = Date.now();
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `recipe/${data}`,
+        Expires: 60 * 5,
+      };
+      console.log(params);
+      const url = await s3.getSignedUrlPromise('putObject', params);
+      urls.push(url);
     }
-  }
-  async deleteImage(file) {
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: `path/${file.originalname}.png`,
-    };
-    s3.deleteObject(params, function (err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data);
-      }
-    });
-  }
-
-  async presignedPUTURL(file) {
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: `path/${file.originalname}.png`,
-      Expires: 60 * 5,
-    };
-    const url = await s3.getSignedUrl('putObject', params);
-    return url;
-  }
-
-  async presignedGETURL(file) {
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: `path/${file.originalname}.png`,
-      Expires: 60 * 5,
-    };
-    const url = await s3.createPresignedPost(params);
-    console.log(url);
-    return url;
+    return urls;
   }
 }
