eb3ab408931ffaa97490eafaec6ad29efc0b0810
Freetargeter <83935992+Freetargeter@users.noreply.github.com>
2021-09-23 21:46:14 +0900

    Update index.ts

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,10 +1,25 @@
 import { combineReducers } from "redux";
 import SignUpAndSignInReducer from "./SignUpAndSignInReducer";
 import IngredientReducer from "./IngredientReducer";
+import WriteRecipePageReducer from "./WriteRecipePageReducer";
+import WriteRecipeContentsReducer from "./WriteRecipeContentsReducer";
+import {
+  SignUpSignInSlide,
+  WriteRecipePage,
+  WriteRecipeContent,
+} from "../types";
+
+export interface RootState {
+  SignUpAndSignInReducer: SignUpSignInSlide;
+  WriteRecipePageReducer: WriteRecipePage;
+  WriteRecipeContentsReducer: WriteRecipeContent;
+}
 
 const rootReducer = combineReducers({
   SignUpAndSignInReducer,
   IngredientReducer,
+  WriteRecipePageReducer,
+  WriteRecipeContentsReducer,
 });
 
 export default rootReducer;
