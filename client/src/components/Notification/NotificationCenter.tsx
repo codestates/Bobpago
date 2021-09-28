cc45c2c23aaabf3fafa9e33b616f6a8f1d6bc1b0
Freetargeter <supreme5876@gmail.com>
2021-09-28 10:28:55 +0900

    [feat]네이버로고추가

--- client/src/components/Notification/NotificationCenter.tsx
+++ client/src/components/Notification/NotificationCenter.tsx
@@ -1,10 +1,19 @@
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Toast from "./Toast";
 import { NotificationContainer } from "./styles";
 import { RootState } from "reducers/index";
+import { resetNotification } from "actions/Notification";
 
 function NofiticationCenter() {
+  const dispatch = useDispatch();
   const state = useSelector((state: RootState) => state.NotificationReducer);
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetNotification());
+    };
+  }, []);
 
   return (
     <NotificationContainer>
