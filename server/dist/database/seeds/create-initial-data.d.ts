1ba13fbd0fba8739ef2ac81c183dec7934a79059
pinion7 <83815628+pinion7@users.noreply.github.com>
2021-09-29 22:24:19 +0900

    Delete server/dist directory

--- server/dist/database/seeds/create-initial-data.d.ts
+++
@@ -1,5 +0,0 @@
-import { Connection } from 'typeorm';
-import { Factory, Seeder } from 'typeorm-seeding';
-export declare class CreatInitialData implements Seeder {
-    run(factory: Factory, connection: Connection): Promise<any>;
-}
--- server/dist/image/image.controller.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="multer" />
-import { ImageService } from './image.service';
-import { ResType } from '../common/response-type';
-export declare class ImageController {
-    private readonly imageService;
-    constructor(imageService: ImageService);
-    upload(id: any, path: any, files: Express.Multer.File): Promise<ResType>;
-    update(id: any, path: any, files: Express.Multer.File): Promise<ResType>;
-}
