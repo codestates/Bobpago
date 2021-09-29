715c6a8f58e5c49689df4a0104bd86f2989a6287
pinion7 <83815628+pinion7@users.noreply.github.com>
2021-09-29 22:24:19 +0900

    Delete server/dist directory

--- server/dist/ingredients/dto/update-ingredient.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateIngredientDto } from './create-ingredient.dto';
-declare const UpdateIngredientDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateIngredientDto>>;
-export declare class UpdateIngredientDto extends UpdateIngredientDto_base {
-}
-export {};
