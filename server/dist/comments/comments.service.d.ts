6f69d7520fe603c9fe5147e0380416cd2723a340
pinion7 <83815628+pinion7@users.noreply.github.com>
2021-09-30 00:01:46 +0900

    Delete server/dist directory

--- server/dist/comments/comments.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
-export declare class CommentsService {
-    create(createCommentDto: CreateCommentDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateCommentDto: UpdateCommentDto): string;
-    remove(id: number): string;
-}
--- server/dist/ingredients/ingredients.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { IngredientsService } from './ingredients.service';
-import { ResType } from 'src/common/response-type';
-export declare class IngredientsController {
-    private readonly ingredientsService;
-    constructor(ingredientsService: IngredientsService);
-    findAll(): Promise<ResType>;
-    getSummary(): Promise<ResType>;
-}
