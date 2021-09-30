13cb99176b0059503f98bfe9a560403175b300d8
vvsoggy <vvsogi@gmail.com>
2021-09-30 16:50:00 +0900

    hotfix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,22 +24,29 @@ function App() {
   const dispatch = useDispatch();
 
   const AllIngredient = async () => {
-    const data = await axios.get(`${process.env.REACT_APP_SERVER_URL}/ingredient`, {
-      withCredentials: true,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const data = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/ingredient/main`,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     dispatch({ type: GET_ALL_DATA, payload: data.data.data });
   };
 
   const SmallIngredient = async () => {
-    const data = await axios.get(`${process.env.REACT_APP_SERVER_URL}/ingredient/summary`, {
-      withCredentials: true,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const data = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/ingredient/basic`,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
     dispatch({ type: GET_SMALL_DATA, payload: data.data.data });
   };
 
