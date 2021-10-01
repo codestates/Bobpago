f65e0aa434e73bc67040eddcae81ffc70603b01f
pinion7 <83815628+pinion7@users.noreply.github.com>
2021-10-01 10:29:25 +0900

    Delete server/dist directory

--- server/dist/ingredients/dto/create-ingredient.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateIngredientDto = void 0;
-class CreateIngredientDto {
-}
-exports.CreateIngredientDto = CreateIngredientDto;
-//# sourceMappingURL=create-ingredient.dto.js.map
